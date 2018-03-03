4e90c32f17d6f130739a144f9daf2dd573306a3d
yntachmad <achmad.t.jayanto@gmail.com>
2018-03-03 15:41:11 +0700

    Initial commit

---
+++ app/src/main/java/com/example/achmadjayanto/pushup/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.achmadjayanto.pushup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void pushUp(){
+        int i = 1+1;
+    }
+}
